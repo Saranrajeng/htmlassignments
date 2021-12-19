fa45cf645569ba74997bd0e30f18109e5dda77fd
Saranrajeng <sraj67487@gmail.com>
2021-12-19 16:41:20 +0530

    finished

---
+++ ballgame/ball.js
@@ -0,0 +1,68 @@
+var ball = document.getElementById("ball");
+var interval;
+var both;
+var bothtop;
+var intervaltop;
+ function moveLeft(){
+    var left = parseInt( window.getComputedStyle(ball).getPropertyValue("left"));
+    if(left>0){
+    ball.style.left= left-2+"px";
+    }
+
+ }
+function moveRight(){
+    var left = parseInt( window.getComputedStyle(ball).getPropertyValue("left"));
+    if(left<420){
+    ball.style.left= left+2+"px";
+    }
+
+}
+document.addEventListener("keydown",action=>{
+    if(both==0){
+        both++;
+    if(action.key==="ArrowLeft"){
+        interval=setInterval(moveLeft,1);
+
+    }
+    if(action.key==="ArrowRight"){
+        interval=setInterval(moveRight,1);
+        
+    }
+}
+});
+document.addEventListener("keyup",action=>{
+    clearInterval(interval);
+    both=0;
+});
+
+function moveup(){
+    var top = parseInt( window.getComputedStyle(ball).getPropertyValue("top"));
+           if(top>0){
+                 ball.style.top= top-2+"px";
+     }
+
+}
+function movedown(){
+var top= parseInt( window.getComputedStyle(ball).getPropertyValue("top"));
+if(top<320){
+ball.style.top= top+2+"px";
+}
+
+}
+document.addEventListener("keydown",action=>{
+if(bothtop==0){
+bothtop++;
+if(action.key==="ArrowUp"){
+intervaltop=setInterval(moveup,1);
+
+}
+if(action.key==="ArrowDown"){
+intervaltop=setInterval(movedown,1);
+
+}
+}
+});
+document.addEventListener("keyup",action=>{
+clearInterval(intervaltop);
+bothtop=0;
+});
